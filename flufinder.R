212d62876f29c5ad80be93b24641938f08c109a7
Kiana Bautista <kebautista@ucsd.edu>
2026-06-03 11:59:47 -0700

    fixed sequence

--- flufinder.R
+++ flufinder.R
@@ -1,14 +1,3 @@
-# Function 2
-
-trypsinize <- function(proteins) {
-  
-  #Opening stringr for simple string manipulation
-  library(stringr)
-  
-  #Using str_split_1 to split proteins after R or K amino acids; "(?<=R|K)" is a regular expression for splitting after "?<=" R or K "R|K"
-  lapply(proteins, str_split_1, pattern="(?<=R|K)")
-}
-
 # Function 1
 upload_fasta <- function(fasta_filename) {
   
@@ -18,4 +7,25 @@ upload_fasta <- function(fasta_filename) {
   read.fasta(fasta_filename, seqtype = "AA", as.string = TRUE,
              set.attributes = FALSE)
 
+}
+
+# Function 2
+trypsinize <- function(proteins) {
+  
+  #Opening stringr for simple string manipulation
+  library(stringr)
+  
+  #Using str_split_1 to split proteins after R or K amino acids; "(?<=R|K)" is a regular expression for splitting after "?<=" R or K "R|K"
+  lapply(proteins, str_split_1, pattern="(?<=R|K)")
+}
+
+# Function 3
+split_peptides <- function(peptides) {
+  
+  # Opening stringr for simple string manipulation
+  library(stringr)
+  
+  #Splitting peptides into individual amino acids using str_split; generates a list of lists of amino acids for each peptide
+  lapply(peptides, str_split, pattern="")
+  
 }
